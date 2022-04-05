0050a35ab8e9e4724677c1d090c286e500f811c2
Shreyansh Sharma <shrey350@gmail.com>
2022-04-05 13:29:09 +0530

    add html report

--- Base/TestBase.cs
+++ Base/TestBase.cs
@@ -1,6 +1,9 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
@@ -9,10 +12,30 @@ namespace testapp.Base
     public class TestBase
     {
         private IWebDriver _driver;
+        private ExtentReports _extentReports;
 
         public TestBase()
         {
             Init();
+            InitReport();
+        }
+
+        [SetUp]
+        public void CreateReport()
+        {
+            var testMethod = TestContext.CurrentContext.Test.MethodName;
+            _extentReports.CreateTest(testMethod);
+        }
+
+        /// <summary>
+        /// Initialize test html report
+        /// </summary>
+        private void InitReport()
+        {
+            var reportPath = Environment.CurrentDirectory + @"\Report\TestReport.html";
+            var htmlReport = new ExtentHtmlReporter(reportPath);
+            _extentReports = new ExtentReports();
+            _extentReports.AttachReporter(htmlReport);
         }
 
         /// <summary>
@@ -42,6 +65,10 @@ namespace testapp.Base
             if (_driver != null)
             {
                 _driver.Quit();
+            }
+            if (_extentReports != null)
+            {
+                _extentReports.Flush();
             }
         }
     }
